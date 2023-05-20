8befeb897aefaff28a6ea88a90c261de38233aa6
Adnankhan10s <adnankhan34r@gmail.com>
2023-05-20 18:58:11 +0500

    Asyncronous

--- index.js
+++ index.js
@@ -4,3 +4,21 @@ let a = "Hello World";
 let b = "I am Good";
 let c = "Great!";
 console.log(a +" "+ b+ " " +c);
+//Asyncronous Function
+
+console.log("start");
+setTimeout( function () {
+    console.log("This is Asyncronous");
+    
+},3000
+
+);
+
+console.log("End")
+
+
+
+
+
+
+
